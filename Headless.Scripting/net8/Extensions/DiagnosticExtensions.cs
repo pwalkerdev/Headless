6651dccb64dac5420fa41470553b49b1fe5af659
Paul Walker <73733025+pwalkerdev@users.noreply.github.com>
2023-12-27 03:04:14 +0800

    fix typo and remove silly comments

--- Headless.Scripting/net8/Extensions/DiagnosticExtensions.cs
+++ Headless.Scripting/net8/Extensions/DiagnosticExtensions.cs
@@ -2,12 +2,12 @@
 // ReSharper disable once CheckNamespace
 namespace Headless.Scripting.Extensions;
 
-internal static class DiagnoticExtensions
+internal static class DiagnosticExtensions
 {
     public static bool IsError(this Diagnostic item) => item.Severity == DiagnosticSeverity.Error;
     public static bool IsNotError(this Diagnostic item) => item.Severity != DiagnosticSeverity.Error;
 
-    public static IEnumerable<Diagnostic> Errors(this IEnumerable<Diagnostic> results) => results.Where(DiagnoticExtensions.IsError);
+    public static IEnumerable<Diagnostic> Errors(this IEnumerable<Diagnostic> results) => results.Where(DiagnosticExtensions.IsError);
     public static bool Success(this IEnumerable<Diagnostic> results) => results.Errors().ToArray().Length == 0;
     public static bool Fail(this IEnumerable<Diagnostic> results) => !results.Success();
 
