75d84c0e774776d4652e7a2896d2d4c691686d97
Paul Walker <73733025+pwalkerdev@users.noreply.github.com>
2024-01-21 12:07:26 +0800

    tidy up of Headless.Scripting.CSharp

--- Headless.CSharp/net8/ScriptParameterDescriptor.cs
+++
@@ -1,7 +0,0 @@
-﻿#if NET8_0_OR_GREATER
-// ReSharper disable once CheckNamespace
-namespace Headless.CSharp;
-
-// ReSharper disable twice NotAccessedPositionalProperty.Global
-public record ScriptParameterDescriptor(int Order, string? Name, Type? Type);
-#endif
---
+++ Headless.CSharp/net8/Scripting/MethodBodyImplementation.cs
@@ -0,0 +1,9 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Headless.CSharp.Scripting;
+
+public enum MethodBodyImplementation
+{
+    Uninitialised,
+    Block,
+    Expression
+}
