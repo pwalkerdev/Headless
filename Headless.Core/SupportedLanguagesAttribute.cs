ef4f8e3b9bf73b36d61242b8adaf632f11fac415
Paul Walker <73733025+pwalkerdev@users.noreply.github.com>
2024-01-24 17:31:49 +0800

    wip

---
+++ Headless.Core/SupportedLanguagesAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Headless.Core
+{
+    public class SupportedLanguagesAttribute : System.Attribute
+    {
+        public string[] SupportedLanguages { get; set; }
+
+        public SupportedLanguagesAttribute(params string[] supportedLanguages)
+        {
+            SupportedLanguages = supportedLanguages;
+        }
+    }
+}
