f964c757d51584f552784fde549c66106428e59e
Paul W <paulw094@gmail.com>
2024-03-30 00:41:50 +0800

    tidyup a bit

---
+++ Headless/Options/CommandLineOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Headless.Options;
+
+public class CommandLineOptions
+{
+    public ScriptInputMode Mode { get; set; }
+    public string Script { get; set; } = string.Empty;
+    public string Postamble { get; set; } = string.Empty;
+}
