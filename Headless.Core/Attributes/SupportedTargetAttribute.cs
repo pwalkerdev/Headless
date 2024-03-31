05dca1424b18b2718edf512c6fc7c0d8048b6d7b
Paul W <73733025+pwalkerdev@users.noreply.github.com>
2024-03-31 23:28:59 +0800

    implemented language version switch to csharp script interpreter and tidied up some bits

---
+++ Headless.Core/Attributes/SupportedTargetAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Headless.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class SupportedTargetAttribute(string name, string version = "latest", string runtime = "any") : Attribute
+    {
+        public string Key { get; } = $"{name}.{version}.{runtime}";
+        public override string ToString() => Key;
+    }
+}
