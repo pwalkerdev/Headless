1e8901e0b96645700627059b26ed6d64317535f5
Paul W <73733025+pwalkerdev@users.noreply.github.com>
2024-04-01 04:25:56 +0800

    fixed some bugs with the DI & language version switch, improved compatibility with older versions of C# and general tidy ups

---
+++ Headless.Core/Attributes/SupportedTargetsAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Headless.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SupportedTargetsAttribute(string name, string versions = "latest", string runtimes = "any") : Attribute
+    {
+        public const char KeyDelimiter = ';';
+        public const char PropertyDelimiter = '|';
+
+        public IEnumerable<string> Keys { get; } = versions.Split(PropertyDelimiter).SelectMany(version => runtimes.Split(PropertyDelimiter).Select(runtime => $"{name}{KeyDelimiter}{version}{KeyDelimiter}{runtime}"));
+    }
+}
