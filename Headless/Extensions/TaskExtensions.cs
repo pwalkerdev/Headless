ae9ca1d3de92c027850d85c6004324df47e97476
Paul W <73733025+pwalkerdev@users.noreply.github.com>
2024-04-14 00:27:09 +0800

    heaps of tidying up of host console app as well as clearing out unused dependencies

---
+++ Headless/Extensions/TaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Headless.Extensions;
+
+internal class TimedResult<TResult>(TResult taskResult, TimeSpan taskDuration)
+{
+    public TResult TaskResult { get; } = taskResult;
+    public TimeSpan TaskDuration { get; } = taskDuration;
+}
+
+internal static class TimedTask
+{
+    public static async Task<TimedResult<TResult>> WithTimer<TResult>(this Task<TResult> task)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        var result = await task;
+        stopwatch.Stop();
+
+        return new TimedResult<TResult>(result, stopwatch.Elapsed);
+    }
+
+    public static Task<TimedResult<TResult>> Run<TResult>(Func<Task<TResult>> task) => Task.Run(task).WithTimer();
+}
