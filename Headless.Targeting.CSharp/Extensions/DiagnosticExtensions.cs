a8dc90490edb3bb33b1558b9da9992947509c06c
Paul W <73733025+pwalkerdev@users.noreply.github.com>
2024-06-29 19:46:04 +0800

    heaps of tidying up / restructuring of the Targetting.CSharp folder. the goal here was to simplify the platform specific code

---
+++ Headless.Targeting.CSharp/Extensions/DiagnosticExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Headless.Targeting.CSharp.Extensions;
+
+internal static class DiagnosticExtensions
+{
+    public static bool IsError(this Diagnostic item) => item.Severity == DiagnosticSeverity.Error;
+    public static bool IsNotError(this Diagnostic item) => item.Severity != DiagnosticSeverity.Error;
+
+    public static IEnumerable<Diagnostic> Errors(this IEnumerable<Diagnostic> results) => results.Where(IsError);
+    public static bool Success(this IEnumerable<Diagnostic> results) => results.Errors().ToArray().Length == 0;
+    public static bool Fail(this IEnumerable<Diagnostic> results) => !results.Success();
+
+#if NET
+    public static string Summarise(this ImmutableArray<Diagnostic> results) => new StringBuilder().AppendJoin(Environment.NewLine, results).ToString();
+#else
+    public static string Summarise(this ImmutableArray<Diagnostic> results) => new StringBuilder().Append(string.Join(Environment.NewLine, results)).ToString();
+#endif
+}
--- Headless.Targeting.CSharp/net8/Syntax/MethodParameter.cs
+++ Headless.Targeting.CSharp/net8/Syntax/MethodParameter.cs
@@ -1,4 +1,4 @@
 ﻿// ReSharper disable once CheckNamespace
-namespace Headless.Targeting.CSharp.Scripting;
+namespace Headless.Targeting.CSharp.Syntax;
 
 internal record MethodParameter(int Order, string? Name, Type? Type);
