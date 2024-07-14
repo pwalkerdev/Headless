fc7d824d4444ab9a33b36e438445b9e24b895a9f
Paul W <paulw094@gmail.com>
2024-07-14 17:41:23 +0800

    updates to script templates, added new EntryPointAttribute and made invoker more robust

--- Headless.Targeting.CSharp/net8/Syntax/SyntaxTreeBuilder.cs
+++ Headless.Targeting.CSharp/net8/Syntax/SyntaxTreeBuilder.cs
@@ -10,6 +10,7 @@ public static class SyntaxTreeBuilder
 {
     public static class {{CLASS_NAME}}
     {
+        [EntryPointAttribute]
         public static object {{METHOD_NAME}}()
         {
             return {{EXPRESSION_IMPLEMENTATION}};
@@ -17,7 +18,7 @@ public static class SyntaxTreeBuilder
     }
 }";
 
-        var script = template.Replace("{{NAMESPACE_NAME}}", "Headless.Framework")
+        var script = template.Replace("{{NAMESPACE_NAME}}", "Headless.Targeting.CSharp.Framework")
             .Replace("{{CLASS_NAME}}", "EmittedExpressionContainerClass")
             .Replace("{{METHOD_NAME}}", "EmittedExpressionContainerMethod")
             .Replace("{{EXPRESSION_IMPLEMENTATION}}", expression);
@@ -32,14 +33,15 @@ public static class SyntaxTreeBuilder
 {
     public static class {{CLASS_NAME}}
     {
+        [EntryPointAttribute]
         public static void {{METHOD_NAME}}()
         {
-            {{STATEMENT_IMPLEMENTATION}}
+            {{STATEMENT_IMPLEMENTATION}};
         }
     }
 }";
 
-        var script = template.Replace("{{NAMESPACE_NAME}}", "Headless.Framework")
+        var script = template.Replace("{{NAMESPACE_NAME}}", "Headless.Targeting.CSharp.Framework")
             .Replace("{{CLASS_NAME}}", "EmittedStatementContainerClass")
             .Replace("{{METHOD_NAME}}", "EmittedStatementContainerMethod")
             .Replace("{{STATEMENT_IMPLEMENTATION}}", statement);
@@ -52,13 +54,14 @@ public static class SyntaxTreeBuilder
         const string template =
 @"namespace {{NAMESPACE_NAME}}
 {
-    public static class {{CLASS_NAME}}
+    public class {{CLASS_NAME}}
     {
+        [EntryPointAttribute]
         {{METHOD_IMPLEMENTATION}}
     }
 }";
 
-        var script = template.Replace("{{NAMESPACE_NAME}}", "Headless.Framework")
+        var script = template.Replace("{{NAMESPACE_NAME}}", "Headless.Targeting.CSharp.Framework")
             .Replace("{{CLASS_NAME}}", "EmittedMethodContainerClass")
             .Replace("{{METHOD_IMPLEMENTATION}}", method);
 
@@ -73,16 +76,22 @@ public static class SyntaxTreeBuilder
     {{CLASS_IMPLEMENTATION}}
 }";
 
-        var script = template.Replace("{{NAMESPACE_NAME}}", "Headless.Framework")
+        var script = template.Replace("{{NAMESPACE_NAME}}", "Headless.Targeting.CSharp.Framework")
             .Replace("{{CLASS_IMPLEMENTATION}}", @class);
 
         return BuildSyntaxTree(script, options);
     }
+
+    // public static SyntaxTree FromClass(string @class, CSharpParseOptions options)
+    // {
+    //     var script = "namespace Headless.Targeting.CSharp.Framework;\r\n\r\n" + @class;
+    //     return BuildSyntaxTree(script, options);
+    // }
 
     public static SyntaxTree FromNamespace(string @namespace, CSharpParseOptions options)
     {
         return BuildSyntaxTree(@namespace, options);
     }
 
-    private static SyntaxTree BuildSyntaxTree(string script, CSharpParseOptions options) => SyntaxFactory.ParseSyntaxTree(script, options);
+    private static SyntaxTree BuildSyntaxTree(string script, CSharpParseOptions options) => SyntaxFactory.ParseSyntaxTree(script, options, encoding: Encoding.UTF8);
 }
